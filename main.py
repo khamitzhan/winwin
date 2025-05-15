1a62420ab86b3c6bf35dd117076b4291f2444952
khamitzhan <khamitzhan@proton.me>
2025-05-15 13:33:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 import os
 from flask import Flask
 
@@ -9,9 +11,7 @@ def home():
 
 if __name__ == "__main__":
     host = os.environ.get("HOST", "0.0.0.0")
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))  # здесь 8000 вместо 5000
     debug = os.environ.get("DEBUG", "False").lower() == "true"
     
     app.run(host=host, port=port, debug=debug)
-
-
