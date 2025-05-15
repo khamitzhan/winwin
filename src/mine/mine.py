9e2fd2447426430259587e3911c17d3f103aba1a
khamitzhan <khamitzhan@proton.me>
2025-05-15 11:51:15 -0700

    Create mine.py

---
+++ src/mine/mine.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(name)
+
+@app.route('/')
+def index():
+    return 'Win-Win Syndicate App Running!'
+
+# Новый маршрут для обработки вебхуков от Kick
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print("Получен вебхук:", data)  # для логов
+    return jsonify({"status": "received"}), 200
+
+if name == "main":
+    app.run(host="0.0.0.0", port=5000)
