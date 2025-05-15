1e2a16cfafa6b7f66d9e76ab6c98f006487e43ac
khamitzhan <khamitzhan@proton.me>
2025-05-15 12:29:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Win-Win Kick MVP работает!"
 
-if name == "main":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
