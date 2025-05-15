a2c64ed1921beaddbebcbb549ecc9419bf717ad5
khamitzhan <khamitzhan@proton.me>
2025-05-15 12:58:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def home():
     return "Win-Win Kick MVP работает!"
 
 if __name__ == "__main__":
+    host = os.environ.get("HOST", "0.0.0.0")
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    debug = os.environ.get("DEBUG", "False").lower() == "true"
+    
+    app.run(host=host, port=port, debug=debug)
+
 
