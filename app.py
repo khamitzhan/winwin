ab016b792d97e390f83d980aa220b70486237d71
khamitzhan <khamitzhan@proton.me>
2025-05-15 11:00:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(name)
@@ -6,11 +7,12 @@ app = Flask(name)
 def index():
     return 'Win-Win Syndicate App Running!'
 
-# Новый маршрут для обработки вебхуков от Kick
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
-    print("Получен вебхук:", data)  # для логов
+    print("Получен вебхук:", data)
     return jsonify({"status": "received"}), 200
-    if name == "main":
-    app.run(host="0.0.0.0", port=5000)
+
+if name == "main":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
