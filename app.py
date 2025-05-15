28c6a4ce14007ae62c69af6de88e1e6836658b87
khamitzhan <khamitzhan@proton.me>
2025-05-15 07:04:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-# Основной backend-файл приложения
+from flask import Flask, request, jsonify
 
-from flask import Flask
-app = Flask(__name__)
+app = Flask(name)
 
 @app.route('/')
 def index():
     return 'Win-Win Syndicate App Running!'
+
+# Новый маршрут для обработки вебхуков от Kick
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print("Получен вебхук:", data)  # для логов
+    return jsonify({"status": "received"}), 200
